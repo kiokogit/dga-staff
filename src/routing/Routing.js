7ada4cb3c29696d0aec02f3ca6d506f359ed5540
kiokogit <kiokovincent12@gmail.com>
2023-01-17 01:16:26 +0300

    redirects

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -1,17 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AdminRouter } from "./AdminRouter";
-import { Error404 } from "../components/application_components/error_boundary/Error404"
+// import { Error404 } from "../components/application_components/error_boundary/Error404"
 
 export const RoutingMain = () => {
 
   return (
     <div>
       <Routes>
-        <Route path='staff/*' element={<AdminRouter />} />
+        <Route path='/staff/*' element={<AdminRouter />} />
         {/* not found route */}
-        <Route path='*' element={<Error404 />} />
+        <Route path='*' element={<Rerouter />} />
       </Routes>
     </div>
+  )
+}
+
+const Rerouter = () => {
+
+  return (
+    <Navigate to={"/staff/login"} />
   )
 }
 
